1807e25d12afc1b992c7553535bb617a10f5b108
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-02-07 11:59:21 +0100

    maj

--- sendemail.php
+++ sendemail.php
@@ -1,49 +1,47 @@
 <?php
-   
 
-    session_cache_limiter( 'nocache' );
-    header( 'Expires: ' . gmdate( 'r', 0 ) );
-    header( 'Content-type: application/json' );
+    session_cache_limiter('nocache');
+    header('Expires: ' . gmdate( 'r',0));
+    header('Content-type: application/json');
 
 
-    $to         = 'emmanuellefevre@protonmail.com';
+    $to = 'contact@emmanuellefevre.com';
     $email_template = 'simple.html';
 
-    $subject    = strip_tags($_POST['subject']);
-    $email      = strip_tags($_POST['email']);
-    $name       = strip_tags($_POST['name']);
-    $message    = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
-    $result     = array();
+    $subject = strip_tags($_POST['subject']);
+    $email   = strip_tags($_POST['email']);
+    $name    = strip_tags($_POST['name']);
+    $message = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
+    $result  = array();
 
 
     if(empty($name)){
 
         $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Erreur!</strong>&nbsp; Le champ nom est vide.' );
-        echo json_encode($result );
+        echo json_encode($result);
         die;
-    } 
+    }
 
     if(empty($email)){
 
         $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Erreur!</strong>&nbsp; Le champ email est vide.' );
-        echo json_encode($result );
+        echo json_encode($result);
         die;
-    } 
+    }
 
     if(empty($subject)){
 
-        $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Erreur!</strong>&nbsp; Vous n\'avez saisi aucun message.' );
-        echo json_encode($result );
+        $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Erreur!</strong>&nbsp; Vous n\'avez saisi aucun sujet.' );
+        echo json_encode($result);
         die;
-   }
+    }
 
     if(empty($message)){
 
-         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Erreur!</strong>&nbsp; Vous n\'avez saisi aucun message.' );
-         echo json_encode($result );
-         die;
+        $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Erreur!</strong>&nbsp; Vous n\'avez saisi aucun message.' );
+        echo json_encode($result);
+        die;
     }
-    
 
 
     $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
@@ -52,11 +50,11 @@
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 
-    $templateTags =  array(
+    $templateTags = array(
         '{{subject}}' => $subject,
-        '{{email}}'=>$email,
-        '{{message}}'=>$message,
-        '{{name}}'=>$name
+        '{{email}}'   => $email,
+        '{{message}}' => $message,
+        '{{name}}'    => $name
         );
 
 
@@ -64,11 +62,10 @@
 
     $contents =  strtr($templateContents, $templateTags);
 
-    if ( mail( $to, $subject, $contents, $headers ) ) {
-        $result = array( 'response' => 'success', 'message'=>'<strong>Merci!</strong>&nbsp; Votre email a bien été envoyé.' );
-    } else {
-        $result = array( 'response' => 'error', 'message'=>'<strong>Erreur!</strong>&nbsp; Votre email n\'a pas été envoyé.'  );
-    }
+    $result = mail($to, $subject, $contents, $headers)
+        ? array('response' => 'success', 'message' => '<strong>Merci!</strong>&nbsp; Votre email a bien été envoyé.')
+        : array('response' => 'error', 'message' => '<strong>Erreur!</strong>&nbsp; Votre email n\'a pas été envoyé.');
+
 
     echo json_encode( $result );
 
