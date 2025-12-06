e4bb760d5375b4c0431b04676260ec3ca2621f77
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-06 14:41:45 +0100

    xp year animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,92 +1,81 @@
 /*
-    = MAP
-    = Preloader
-    = Full Screen Slider
-    = Typing Text JS
-    = Sticky Menu
-    = Back To Top
-    = Countup
-    = Progress Bar
-    = More skill
-    = Shuffle
-    = Magnific Popup
+  = MAP
+  = Preloader
+  = Full Screen Slider
+  = Typing Text JS
+  = Sticky Menu
+  = Back To Top
+  = Countup
+  = Progress Bar
+  = More skill
+  = Shuffle
+  = Magnific Popup
 */
 
-
 /* ---------------------------------------------- /*
-    * MAP
+  * MAP
 /* ---------------------------------------------- */
 
 var mymap = L.map('mapCanvas').setView([43.833328, -0.56667], 9);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-    attribution: '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: 1,
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+  attribution: '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
+  minZoom: 1,
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
 var marqueur = L.marker([45.139429, -0.446113]).addTo(mymap);
 marqueur.bindPopup("<p>"+ 'Saint-Savin '+"</p>").openPopup();
 var circle = L.circle([44.838180, -0.57900], {
-    color: 'rgba(255, 0, 0, 0.7)',
-    fillColor: '#f03',
-    fillOpacity: 0.35,
-    radius: 12000
+  color: 'rgba(255, 0, 0, 0.7)',
+  fillColor: '#f03',
+  fillOpacity: 0.35,
+  radius: 12000
 }).addTo(mymap);
 
 
-
 jQuery(function ($) {
+  'use strict';
 
-    'use strict';
+  /* ---------------------------------------------- /*
+    * Preloader
+  /* ---------------------------------------------- */
 
-    /* ---------------------------------------------- /*
-     * Preloader
-    /* ---------------------------------------------- */
+  $(window).ready(function() {
+    $('#pre-status').fadeOut();
+    $('#tt-preloader').delay(350).fadeOut('slow');
+  });
 
-    $(window).ready(function() {
-        $('#pre-status').fadeOut();
-        $('#tt-preloader').delay(350).fadeOut('slow');
+  // -------------------------------------------------------------
+  // Full Screen Slider
+  // -------------------------------------------------------------
+  (function () {
+    $(".tt-fullHeight").height($(window).height());
+    $(window).resize(function(){
+      $(".tt-fullHeight").height($(window).height());
     });
+  }());
 
+  // -------------------------------------------------------------
+  // Typing Text JS
+  // -------------------------------------------------------------
+  // get the element
+  const text = document.querySelector('.typing-text-js');
 
-
-    // -------------------------------------------------------------
-    // Full Screen Slider
-    // -------------------------------------------------------------
-    (function () {
-        $(".tt-fullHeight").height($(window).height());
-
-        $(window).resize(function(){
-            $(".tt-fullHeight").height($(window).height());
-        });
-
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Typing Text JS
-    // -------------------------------------------------------------
-
-    // get the element
-    const text = document.querySelector('.typing-text-js');
-
-    // make a words array
-    const words = [
+  // make a words array
+  const words = [
     "Applicatif",
     "Web"
-    ];
+  ];
 
-    // start typing effect
-    setTyper(text, words);
+  // start typing effect
+  setTyper(text, words);
 
-    function setTyper(element, words) {
-
+  function setTyper(element, words) {
     const LETTER_TYPE_DELAY = 75;
     const WORD_STAY_DELAY = 2000;
 
@@ -102,270 +91,247 @@ jQuery(function ($) {
     startTyping();
 
     function startTyping() {
-        wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+      wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
     }
 
     function typeLetter() {
-        const word = words[wordIndex];
+      const word = words[wordIndex];
 
-        if (direction == DIRECTION_FORWARDS) {
-        letterIndex++;
+      if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
 
         if (letterIndex == word.length) {
-            direction = DIRECTION_BACKWARDS;
-            clearInterval(wordTypeInterval);
-            setTimeout(startTyping, WORD_STAY_DELAY);
+          direction = DIRECTION_BACKWARDS;
+          clearInterval(wordTypeInterval);
+          setTimeout(startTyping, WORD_STAY_DELAY);
         }
-
-        } else if (direction == DIRECTION_BACKWARDS) {
-        letterIndex--;
+      }
+      else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
 
         if (letterIndex == 0) {
-            nextWord();
+          nextWord();
         }
-        }
+      }
 
-        const textToType = word.substring(0, letterIndex);
-
-        element.textContent = textToType;
+      const textToType = word.substring(0, letterIndex);
+      element.textContent = textToType;
     }
 
     function nextWord() {
+      letterIndex = 0;
+      direction = DIRECTION_FORWARDS;
+      wordIndex++;
 
-        letterIndex = 0;
-        direction = DIRECTION_FORWARDS;
-        wordIndex++;
-
-        if (wordIndex == words.length) {
+      if (wordIndex == words.length) {
         wordIndex = 0;
-        }
-
+      }
     }
-    }
+  }
 
-
-
-    // -------------------------------------------------------------
-    // Sticky Menu
-    // -------------------------------------------------------------
-
-    (function () {
-        $('.header').sticky({
-            topSpacing: 0
-        });
-
-        $('body').scrollspy({
-            target: '.navbar-custom',
-            offset: 70
-        })
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Back To Top
-    // -------------------------------------------------------------
-
-    (function () {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $('.scroll-up').fadeIn();
-            } else {
-                $('.scroll-up').fadeOut();
-            }
-        });
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Countup
-    // -------------------------------------------------------------
-    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $(this).find('.timer').each(function () {
-                var $this = $(this);
-                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.ceil(this.Counter));
-                    }
-                });
-            });
-            $(this).unbind('inview');
-        }
+  // -------------------------------------------------------------
+  // Sticky Menu
+  // -------------------------------------------------------------
+  (function () {
+    $('.header').sticky({
+      topSpacing: 0
     });
+    $('body').scrollspy({
+      target: '.navbar-custom',
+      offset: 70
+    })
+  }());
 
-
-
-    // -------------------------------------------------------------
-    // Progress Bar
-    // -------------------------------------------------------------
- 
-    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $.each($('div.progress-bar'),function(){
-                $(this).css('width', $(this).attr('aria-valuenow')+'%');
-            });
-            $(this).unbind('inview');
-        }
+  // -------------------------------------------------------------
+  // Back To Top
+  // -------------------------------------------------------------
+  (function () {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.scroll-up').fadeIn();
+      }
+      else {
+        $('.scroll-up').fadeOut();
+      }
     });
-    
+  }());
 
-
-    // -------------------------------------------------------------
-    // More skill
-    // -------------------------------------------------------------
-    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $('.chart').easyPieChart({
-                //your configuration goes here
-                easing: 'easeOut',
-                delay: 3000,
-                barColor:'#68c3a3',
-                trackColor:'rgba(255,255,255,0.2)',
-                scaleColor: false,
-                lineWidth: 8,
-                size: 140,
-                animate: 2000,
-                onStep: function(from, to, percent) {
-                    this.el.children[0].innerHTML = Math.round(percent);
-                }
-
-            });
-            $(this).unbind('inview');
-        }
-    });
-
-
-
-    // -------------------------------------------------------------
-    // Shuffle
-    // -------------------------------------------------------------
-
-    (function () {
-
-        var $grid = $('#grid');
-
-        $grid.shuffle({
-            itemSelector: '.portfolio-item'
-        });
-
-        /* reshuffle when user clicks a filter item */
-        $('#filter a').click(function (e) {
-            e.preventDefault();
-
-            // set active class
-            $('#filter a').removeClass('active');
-            $(this).addClass('active');
-
-            // get group name from clicked item
-            var groupName = $(this).attr('data-group');
-
-            // reshuffle grid
-            $grid.shuffle('shuffle', groupName );
-        });
-
-
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Magnific Popup
-    // -------------------------------------------------------------
-
-    (function () {
-      $('.image-link').magnificPopup({
-
-        gallery: {
-          enabled: true
-        },
-        removalDelay: 300, // Delay in milliseconds before popup is removed
-        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-        type:'image'
-      });
-
-    }());
-
-    (function () {
-        $('.popup-video').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-with-zoom',
-            removalDelay: 300,
-            preloader: false,
-            fixedContentPos: false
-        });
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // STELLAR FOR BACKGROUND SCROLLING
-    // -------------------------------------------------------------
-
-    $(window).load(function() {
-
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-         
-        }else {
-            $.stellar({
-                horizontalScrolling: false,
-                responsive: true
-            });
-        }
-
-    });
-
-
-
-    // -------------------------------------------------------------
-    // WOW JS
-    // -------------------------------------------------------------
-
-    (function () {
-
-        new WOW({
-
-            mobile:  false
-
-        }).init();
-
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Contact Form
-    // -------------------------------------------------------------
-
-    $('#contactForm').on('submit',function(e){
-
-        e.preventDefault();
-
-        var $action = $(this).prop('action');
-        var $data = $(this).serialize();
+  // -------------------------------------------------------------
+  // Countup
+  // -------------------------------------------------------------
+  $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    if (visible) {
+      $(this).find('.timer').each(function () {
         var $this = $(this);
 
-        $this.prevAll('.alert').remove();
+        var $parentBlock = $this.closest('.fill-hover');
 
-        $.post( $action, $data, function( data ) {
+        if (!$this.data('target')) {
+          $this.data('target', $this.text());
+        }
 
-            if( data.response=='error' ){
+        var targetVal = $this.data('target');
 
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
-            }
+        $this.stop();
 
-            if( data.response=='success' ){
+        $({ Counter: 0 }).animate({ Counter: targetVal }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter));
+          },
+          complete: function() {
+            $this.text(targetVal);
+            $parentBlock.addClass('active-orange');
+            $this.addClass('timer-pop');
+            setTimeout(function() {
+              $this.removeClass('timer-pop');
+            }, 500);
+          }
+        });
+      });
+    }
+    else {
+      $(this).find('.timer').each(function () {
+        var $this = $(this);
+        var $parentBlock = $this.closest('.fill-hover');
+        $this.stop();
+        $parentBlock.removeClass('active-orange');
+        $this.removeClass('timer-pop');
+        $this.text('0');
+      });
+    }
+  });
 
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
-                $this.find('input, textarea').val('');
-            }
+  // -------------------------------------------------------------
+  // Progress Bar
+  // -------------------------------------------------------------
+  $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    if (visible) {
+      $.each($('div.progress-bar'),function(){
+        $(this).css('width', $(this).attr('aria-valuenow')+'%');
+      });
+      $(this).unbind('inview');
+    }
+  });
 
-        }, "json");
+  // -------------------------------------------------------------
+  // More skill
+  // -------------------------------------------------------------
+  $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    if (visible) {
+      $('.chart').easyPieChart({
+        //your configuration goes here
+        easing: 'easeOut',
+        delay: 3000,
+        barColor:'#68c3a3',
+        trackColor:'rgba(255,255,255,0.2)',
+        scaleColor: false,
+        lineWidth: 8,
+        size: 140,
+        animate: 2000,
+        onStep: function(from, to, percent) {
+          this.el.children[0].innerHTML = Math.round(percent);
+        }
+      });
+      $(this).unbind('inview');
+    }
+  });
 
+  // -------------------------------------------------------------
+  // Shuffle
+  // -------------------------------------------------------------
+  (function () {
+    var $grid = $('#grid');
+
+    $grid.shuffle({
+        itemSelector: '.portfolio-item'
     });
 
+    /* reshuffle when user clicks a filter item */
+    $('#filter a').click(function (e) {
+      e.preventDefault();
+
+      // set active class
+      $('#filter a').removeClass('active');
+      $(this).addClass('active');
+
+      // get group name from clicked item
+      var groupName = $(this).attr('data-group');
+
+      // reshuffle grid
+      $grid.shuffle('shuffle', groupName );
+    });
+  }());
+
+  // -------------------------------------------------------------
+  // Magnific Popup
+  // -------------------------------------------------------------
+  (function () {
+    $('.image-link').magnificPopup({
+      gallery: {
+        enabled: true
+      },
+      removalDelay: 300, // Delay in milliseconds before popup is removed
+      mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+      type:'image'
+    });
+  }());
+
+  (function () {
+    $('.popup-video').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-with-zoom',
+      removalDelay: 300,
+      preloader: false,
+      fixedContentPos: false
+    });
+  }());
+
+  // -------------------------------------------------------------
+  // STELLAR FOR BACKGROUND SCROLLING
+  // -------------------------------------------------------------
+  $(window).load(function() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {}
+    else {
+      $.stellar({
+        horizontalScrolling: false,
+        responsive: true
+      });
+    }
+  });
+
+  // -------------------------------------------------------------
+  // WOW JS
+  // -------------------------------------------------------------
+  (function () {
+    new WOW({
+      mobile:  false
+    }).init();
+  }());
+
+  // -------------------------------------------------------------
+  // Contact Form
+  // -------------------------------------------------------------
+  $('#contactForm').on('submit',function(e){
+    e.preventDefault();
+
+    var $action = $(this).prop('action');
+    var $data = $(this).serialize();
+    var $this = $(this);
+
+    $this.prevAll('.alert').remove();
+
+    $.post( $action, $data, function( data ) {
+      if( data.response=='error' ){
+        $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
+      }
+
+      if( data.response=='success' ){
+        $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
+        $this.find('input, textarea').val('');
+      }
+    }, "json");
+  });
 });
